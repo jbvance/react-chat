f36e96990c2e42a0969f81371fccb058d0725c75
Jason Vance <jbvance@gmail.com>
2017-04-25 15:19:04 -0500

    Create views

--- src/App.js
+++ src/App.js
@@ -51,8 +51,77 @@ class App extends React.Component {
   render(){
     const messages = store.getState().messages;
     return (
-      <div class='ui segment'>
-        Messages go here
+      <div className='ui segment'>
+        <MessageView messages={messages} />
+        <MessageInput />
+      </div>
+    );
+  }
+}
+
+class MessageInput extends React.Component{
+  state = {
+    value: '',
+  };
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  };
+
+  handleSubmit = () => {
+    store.dispatch({
+      type: 'ADD_MESSAGE',
+      message: this.state.value,
+    });
+    this.setState({
+      value: '',
+    })
+  };
+
+  render() {
+    return (
+      <div className='ui input'>
+        <input
+          onChange={this.onChange}
+          value={this.state.value}
+          type='text'
+        />
+        <button
+          onClick={this.handleSubmit}
+          className='ui primary button'
+          type='submit'
+        >
+          Submit
+        </button>
+      </div>
+    )
+  }
+
+}
+
+class MessageView extends React.Component {
+  handleClick = (index) => {
+    store.dispatch({
+      type: 'DELETE_MESSAGE',
+      index: index,
+    });
+  };
+
+  render() {
+    const messages = this.props.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(index)}
+      >
+        {message}
+      </div>
+    ));
+    return (
+      <div className='ui comments'>
+        {messages}
       </div>
     );
   }
