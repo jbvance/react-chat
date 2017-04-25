1370145951c69264aa81a23b52a48f4b6b79f75f
Jason Vance <jbvance@gmail.com>
2017-04-25 14:55:13 -0500

    Add Redux functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-function createStore(reducer) {
-  let state = 0;
+import React from 'react';
+
+function createStore(reducer, initialState) {
+  let state = initialState;
+  const listeners = [];
+
+  const subscribe = (listener) => (
+    listeners.push(listener)
+  );
 
   const getState = () => (state);
 
   const dispatch = (action) => {
     state = reducer(state, action);
+    listeners.forEach(l => l());
   };
 
   return {
+    subscribe,
     getState,
     dispatch,
   };
 }
+
+function reducer(state, action){
+  if (action.type === 'ADD_MESSAGE'){
+    return {
+      messages: state.messages.concat(action.message),
+    }
+  } else if (action.type === 'DELETE_MESSAGE'){
+    return {
+      messages: [
+        ...state.messages.slice(0, action.index),
+        ...state.messages.slice (action.index + 1, state.messages.length)
+      ]
+    };
+  } else {
+    return state;
+  }
+}
+
+const initialState = { messages: [] };
+
+const store = createStore(reducer, initialState);
+
+class App extends React.Component {
+  componentDidMount(){
+    store.subscribe(() => this.forceUpdate());
+  }
+
+  render(){
+    const messages = store.getState().messages;
+    return (
+      <div class='ui segment'>
+        Messages go here
+      </div>
+    );
+  }
+}
+
+export default App;
